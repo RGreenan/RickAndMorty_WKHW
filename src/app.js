efc03d6773dfe732a4cadaa5ace1146a96361b78
Ray <ray.greenan.89@outlook.com>
2018-12-02 12:54:53 +0000

    Basic file structure added

---
+++ src/app.js
@@ -0,0 +1,8 @@
+const Characters = require('./models/characters.js')
+
+document.addEventListener('DOMContentLoaded', () => {
+  console.log('JavaScript Loaded');
+
+  const characterContainer = document.querySelector('.index-contents');
+
+})
---
+++ src/models/characters.js
@@ -0,0 +1,17 @@
+const PubSub = require('../helpers/pub_sub.js');
+const RequestHelper = require('../helpers/request_helper.js');
+
+const Characters = function () {
+  this.data = null;
+};
+
+Characters.prototype.getData = function () {
+  const requestHelper = new RequestHelper('https://rickandmortyapi.com/api/character/');
+  requestHelper.get()
+  .then((characters) => {
+    this.data = characters;
+    PubSub.publish('Characters:character-list-ready', this.data);
+  });
+};
+
+module.exports = Characters;
